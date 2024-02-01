a38962a9e472d8adc467986b01a2ee4997f2076f
Kishna Bansal <kb.20051981@gmail.com>
2024-02-02 01:19:35 +0530

    Added gradient background and made the grid items to have circular effect and used InkWell widget to show a splash effect which is also in circular wave form and added padding for gridview items and for overall app also

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,18 +12,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick your Category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           mainAxisSpacing: 20,
           crossAxisSpacing: 20,
         ),
-        children: const [
-          Text("1"),
-          Text("2"),
-          Text("3"),
-          Text("4"),
-          Text("5"),
+        children: [
+          //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for(final category in availableCategories) 
+            CategoryGridItem(category: category) 
         ],
       ),
     );
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart';
+//import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({required this.category, super.key});
@@ -8,6 +9,26 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Placeholder();
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColorDark,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
+      ),
+    );
   }
 }
